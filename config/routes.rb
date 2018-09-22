31a21c85e044366b443862e863258e17ffc16839
beyond2013 <imran.shahjee@gmail.com>
2018-09-22 12:13:19 +0500

    root route defined

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :couplets
   resources :poets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root :to => 'couplets#index'
 end
