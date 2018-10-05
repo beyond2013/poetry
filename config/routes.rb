e0dd404780e924f7bbb7348f39c616cedbac15f1
beyond2013 <imran.shahjee@gmail.com>
2018-10-05 22:32:38 +0500

    add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :couplets
   resources :poets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root :to => 'couplets#index'
+	root :to => 'welcome#index'
 end
