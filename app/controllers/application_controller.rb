79b06bd861af84870f1498eb86265ec99c5c9c5c
Imran Ali <imran.cs.uob@gmail.com>
2020-08-27 13:32:18 +0500

    only authenticated users for insert, and update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!, except: [:index, :show]
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
