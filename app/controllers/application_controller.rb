9116d730e1bfc00f073b68221f71ed9a21327025
beyond2013 <imran.cs.uob@gmail.com>
2021-08-19 12:29:47 +0500

    removed devise and omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!, except: [:index, :show]
+  #before_action :authenticate_user!, except: [:index, :show]
 
-  private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-helper_method :current_user
+  # private
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+# helper_method :current_user
 end
